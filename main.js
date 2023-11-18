a1c4746951bc2cc9e89ca7495277d10d6d57068a
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-11-18 17:59:51 +0330

    feat: Reworked logic, Added Reset

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 import './style.css'
-import { stopwatch,stop } from './timer'
+import { stopwatch,reset } from './timer'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <div id="timer">00:00:000</div>
     <button id="strt">Start</button>
     <button id="stp">Stop</button>
+    <button id="rst">Reset</button>
   </div>
 `
 let flag = false;
 
 document.getElementById("strt").addEventListener('click', stopwatch);
-document.getElementById("stp").addEventListener('click', stop);
+document.getElementById("rst").addEventListener('click', reset);
