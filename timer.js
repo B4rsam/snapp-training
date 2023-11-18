ab1eb5fe956950315c0faf8c2dfa65cd76d564fa
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-11-18 17:47:57 +0330

    fix: fixed clear

--- timer.js
+++ timer.js
@@ -1,9 +1,9 @@
-let interval;
+let interval,date;
 
 export function stopwatch(event) {
   console.log(event);
   interval = setInterval(() => {
-    const date = new Date();
+    date = new Date();
     document.getElementById("timer").innerHTML = "00" + ":" + date.getSeconds() + ":" + date.getMilliseconds();
   }, 10)
 }
@@ -11,5 +11,5 @@ export function stopwatch(event) {
 export function stop(event) {
   console.log(event);
   document.getElementById("timer").innerHTML = "00:00:000";
-  interval = clearInterval();
+  clearInterval(interval);
 }
