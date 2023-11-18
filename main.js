ae4b3624335af2b35c1669802920da9eeb1e01d5
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-11-18 17:24:02 +0330

    feat: added timer logic (unfinished)

--- main.js
+++ main.js
@@ -3,20 +3,12 @@ import { stopwatch,stop } from './timer'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <div id="timer"></div>
+    <div id="timer">00:00:000</div>
     <button id="strt">Start</button>
     <button id="stp">Stop</button>
   </div>
 `
 let flag = false;
-let time = {
-  mil: 0,
-  sec: 0,
-  min: 0,
-}
 
-
-document.getElementById("strt").addEventListener('click', () => stopwatch(flag));
-document.getElementById("stp").addEventListener('click', () => stop(flag));
-
-document.getElementById("timer").innerHTML = time.min + ":" + time.sec + ":" + time.mil;
+document.getElementById("strt").addEventListener('click', stopwatch);
+document.getElementById("stp").addEventListener('click', stop);
--- timer.js
+++ timer.js
@@ -1,9 +1,15 @@
-export function stopwatch(flag) {
-  console.log("stpwtch");
-  flag = true;
+let interval;
+
+export function stopwatch(event) {
+  console.log(event);
+  interval = setInterval(() => {
+    const date = new Date();
+    document.getElementById("timer").innerHTML = "00" + ":" + date.getSeconds() + ":" + date.getMilliseconds();
+  }, 10)
 }
 
-export function stop(flag) {
-  console.log("stp");
-  flag = false;
+export function stop(event) {
+  console.log(event);
+  document.getElementById("timer").innerHTML = "00:00:000";
+  interval = clearInterval();
 }
