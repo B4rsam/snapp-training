3e2dd070ed5f71f7322ff96210d1026172dbff7a
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-11-18 14:44:26 +0330

    feat: redone and finished HTML elements

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ import { stopwatch,stop } from './timer'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <div id="timer">0</div>
+    <div id="timer"></div>
     <button id="strt">Start</button>
     <button id="stp">Stop</button>
   </div>
 `
 let flag = false;
+let time = {
+  mil: 0,
+  sec: 0,
+  min: 0,
+}
+
 
 document.getElementById("strt").addEventListener('click', () => stopwatch(flag));
 document.getElementById("stp").addEventListener('click', () => stop(flag));
+
+document.getElementById("timer").innerHTML = time.min + ":" + time.sec + ":" + time.mil;
