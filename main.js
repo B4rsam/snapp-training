f4932f5c1fa7ddd5554f5db3a436d5da3d0357d4
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-11-20 16:30:08 +0330

    feat: added api.js + HTML content

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import './style.css'
+import {getList} from './api.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    
+    <button id="listButton">See List</button>
   </div>
 `
+
+document.getElementById("listButton").addEventListener('click',getList);
