3610cad0315939ac95a66977b818ee004a6edd67
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-11-20 16:19:04 +0330

    initial commit

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
 import './style.css'
-import { stopwatch,reset,stop } from './timer'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <div id="timer">00:00:000</div>
-    <button id="strt">Start</button>
-    <button id="stp">Stop</button>
-    <button id="rst">Reset</button>
+    
   </div>
 `
-let flag = false;
-
-document.getElementById("strt").addEventListener('click', stopwatch);
-document.getElementById("rst").addEventListener('click', reset);
-document.getElementById("stp").addEventListener('click', stop);
