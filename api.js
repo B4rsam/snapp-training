4452ae0d99b5441c8ce074924effb2962acbb956
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-11-22 17:10:39 +0330

    feat: further styling work

--- api.js
+++ api.js
@@ -8,24 +8,34 @@ export function getList() {
                 const card = document.createElement("div");
                 const cardTitle = document.createElement("h4");
                 const cardBody = document.createElement("p");
+                const cardIdButtonBox = document.createElement("div");
                 const cardId = document.createElement("h5");
                 const cardUserId = document.createElement("h5");
 
+                cardTitle.classList.add("cardTitle");
+                cardBody.classList.add("cardBody");
+                cardId.classList.add("classId");
+                cardUserId.classList.add("classUserId");
+                cardIdButtonBox.classList.add("cardBox");
+
                 cardTitle.textContent += item.title;
                 cardBody.textContent += item.body;
-                cardId.textContent += "ID: " + item.id;
+                cardId.textContent += "ID: " + item.id + " ";
                 cardUserId.textContent += "User ID: " + item.userId;
 
                 card.appendChild(cardTitle);
                 card.appendChild(cardBody);
-                card.appendChild(cardId);
-                card.appendChild(cardUserId);
+                cardIdButtonBox.appendChild(cardId);
+                cardIdButtonBox.appendChild(cardUserId);
+                card.appendChild(cardIdButtonBox);
+
                 card.classList.add("card");
                 return card;
             }
              )
              list.forEach(element => {
                 cardList.appendChild(element);
+                cardList.classList.add("cardList");
              });
              console.log(cardList);
              document.getElementById("cardArea").appendChild(cardList); 
