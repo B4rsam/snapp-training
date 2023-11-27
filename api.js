bef724c2607d9885c56e21ec14a521125fe7f14f
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-11-27 14:42:40 +0330

    chore: ran prettier

--- api.js
+++ api.js
@@ -1,65 +1,63 @@
-
 export function getList() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => response.json())
-        .then(json => {
-            let i = 0;
-            const cardList = document.createElement("div");
-            const list = json.map((item) => {
-                const card = document.createElement("div");
-                const cardTitle = document.createElement("h4");
-                const cardBody = document.createElement("p");
-                const cardIdButtonBox = document.createElement("div");
-                const cardId = document.createElement("h5");
-                const cardUserId = document.createElement("h5");
-                const cardButton = document.createElement("button");
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => {
+      let i = 0;
+      const cardList = document.createElement("div");
+      const list = json.map((item) => {
+        const card = document.createElement("div");
+        const cardTitle = document.createElement("h4");
+        const cardBody = document.createElement("p");
+        const cardIdButtonBox = document.createElement("div");
+        const cardId = document.createElement("h5");
+        const cardUserId = document.createElement("h5");
+        const cardButton = document.createElement("button");
 
-                cardTitle.classList.add("cardTitle");
-                cardBody.classList.add("cardBody");
-                cardId.classList.add("classId");
-                cardUserId.classList.add("classUserId");
-                cardIdButtonBox.classList.add("cardBox");
-                cardButton.classList.add("cardButton");
+        cardTitle.classList.add("cardTitle");
+        cardBody.classList.add("cardBody");
+        cardId.classList.add("classId");
+        cardUserId.classList.add("classUserId");
+        cardIdButtonBox.classList.add("cardBox");
+        cardButton.classList.add("cardButton");
 
-                cardTitle.textContent += item.title;
-                cardBody.textContent += item.body;
-                cardId.textContent += "ID: " + item.id + " ";
-                cardUserId.textContent += "User ID: " + item.userId;
-                cardButton.textContent = "Delete Entry";
+        cardTitle.textContent += item.title;
+        cardBody.textContent += item.body;
+        cardId.textContent += "ID: " + item.id + " ";
+        cardUserId.textContent += "User ID: " + item.userId;
+        cardButton.textContent = "Delete Entry";
 
-                card.appendChild(cardTitle);
-                card.appendChild(cardBody);
-                cardIdButtonBox.appendChild(cardId);
-                cardIdButtonBox.appendChild(cardUserId);
-                card.appendChild(cardIdButtonBox);
-                card.appendChild(cardButton);
+        card.appendChild(cardTitle);
+        card.appendChild(cardBody);
+        cardIdButtonBox.appendChild(cardId);
+        cardIdButtonBox.appendChild(cardUserId);
+        card.appendChild(cardIdButtonBox);
+        card.appendChild(cardButton);
 
-                card.id = "card" + i;
-                cardButton.id = "button" + i;
+        card.id = "card" + i;
+        cardButton.id = "button" + i;
 
-                card.classList.add("card");
-                i++;
+        card.classList.add("card");
+        i++;
 
-                cardButton.addEventListener("click", deleteCard);
+        cardButton.addEventListener("click", deleteCard);
 
-                return card;
-            }
-             )
-             list.forEach(element => {
-                cardList.appendChild(element);
-                cardList.classList.add("cardList");
-             });
-             console.log(cardList);
-             document.getElementById("cardArea").appendChild(cardList);
-        })
-        .catch(console.log("fail"));
+        return card;
+      });
+      list.forEach((element) => {
+        cardList.appendChild(element);
+        cardList.classList.add("cardList");
+      });
+      console.log(cardList);
+      document.getElementById("cardArea").appendChild(cardList);
+    })
+    .catch(console.log("fail"));
 }
 
 function deleteCard() {
-    console.log("Delete called from button id: " + this.id)
-    let id = toString(this.id)
-    document.getElementById(this.id).parentElement.remove();
-    fetch("https://jsonplaceholder.typicode.com/posts/"+ id.match(/(\d+)/) , {
-        method: 'DELETE',
-    })
+  console.log("Delete called from button id: " + this.id);
+  let id = toString(this.id);
+  document.getElementById(this.id).parentElement.remove();
+  fetch("https://jsonplaceholder.typicode.com/posts/" + id.match(/(\d+)/), {
+    method: "DELETE",
+  });
 }
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import './style.css'
-import {getList} from './api.js'
+import "./style.css";
+import { getList } from "./api.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <button id="listButton">See List</button>
     <div id="cardArea"></div>
   </div>
-`
+`;
 
-document.getElementById("listButton").addEventListener('click',getList);
+document.getElementById("listButton").addEventListener("click", getList);
