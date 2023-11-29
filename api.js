9a0e33b40abdaf39d1940f0249f8f54e49be79b4
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-11-29 16:00:27 +0330

    feat: rewrote getList

--- api.js
+++ api.js
@@ -3,9 +3,23 @@ export function getList() {
     .then((response) => response.json())
     .then((json) => {
       let i = 0;
-      const cardList = document.createElement("div");
+      //const cardList = document.createElement("div");
+      //cardList.classList.add("cardlist");
+      const cardArea = document.getElementById("cardArea");
       const list = json.map((item) => {
         const card = document.createElement("div");
+        card.id = "card" + i;
+        card.innerHTML = `
+          <h4 class="cardTitle">${item.title}</h4>
+          <p class="cardBody">${item.body}</p>
+          <div class="cardBox">
+            <h5 class="classUserId">User ID: ${item.userId}</h5>
+            <h5 class="classId">ID: ${item.id}</h5>
+          </div>
+          <button class="cardButton" id="button${i}" onclick="${() => deleteCard}">Delete Entry</button>
+          `;
+        i++;
+        /*const card = document.createElement("div");
         const cardTitle = document.createElement("h4");
         const cardBody = document.createElement("p");
         const cardIdButtonBox = document.createElement("div");
@@ -39,25 +53,24 @@ export function getList() {
         card.classList.add("card");
         i++;
 
-        cardButton.addEventListener("click", deleteCard);
-
+        cardButton.addEventListener("click", deleteCard); */
         return card;
       });
       list.forEach((element) => {
-        cardList.appendChild(element);
-        cardList.classList.add("cardList");
+        cardArea.appendChild(element);
+        //cardList.classList.add("cardArea");
       });
-      console.log(cardList);
-      document.getElementById("cardArea").appendChild(cardList);
-    })
-    .catch(console.log("fail"));
+      //console.log(cardList);
+      //document.getElementById("cardArea").appendChild(cardList);
+    }).catch(() => {console.log("fail")});
 }
 
 function deleteCard() {
   console.log("Delete called from button id: " + this.id);
   let id = toString(this.id);
-  document.getElementById(this.id).parentElement.remove();
+  const parent = document.getElementById(this.id).parentElement;
   fetch("https://jsonplaceholder.typicode.com/posts/" + id.match(/(\d+)/), {
     method: "DELETE",
-  });
+  }).then(() => {console.log("Deleted element: " + this.id);
+                 if (parent) {parent.remove()}});
 }
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import "./style.css";
 import { getList } from "./api.js";
-
+const name = 'See list';
 document.querySelector("#app").innerHTML = `
   <div>
-    <button id="listButton">See List</button>
+    <button id="listButton">${name}</button>
     <div id="cardArea"></div>
   </div>
 `;
